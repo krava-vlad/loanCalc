e6747500d8f2eff37041f4a07a05c98b20b53307
krava-vlad <krava.vlad.kvn@gmail.com>
2021-01-21 14:03:41 -0800

    Fix: fixing the font issue on calculator

--- src/components/calc.js
+++ src/components/calc.js
@@ -151,6 +151,7 @@ export default class calc extends Component {
               value="PPPFunds2020Yes"
               checked={recievedPPPFunds2020 === 'PPPFunds2020Yes'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -162,6 +163,7 @@ export default class calc extends Component {
               value="PPPFunds2020No"
               checked={recievedPPPFunds2020 === 'PPPFunds2020No'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -180,6 +182,7 @@ export default class calc extends Component {
               value="BussinessFeb2020Yes"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020Yes'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -191,6 +194,7 @@ export default class calc extends Component {
               value="BussinessFeb2020No"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020No'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -209,6 +213,7 @@ export default class calc extends Component {
               value="SeasonalBusinessYes"
               checked={isSeasonalBusiness === 'SeasonalBusinessYes'}
               onChange={this.handleSeasonalBusiness}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -220,6 +225,7 @@ export default class calc extends Component {
               value="SeasonalBusinessNo"
               checked={isSeasonalBusiness === 'SeasonalBusinessNo'}
               onChange={this.handleSeasonalBusiness}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -266,6 +272,7 @@ export default class calc extends Component {
               value="PPPFunds2020Yes"
               checked={recievedPPPFunds2020 === 'PPPFunds2020Yes'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -277,6 +284,7 @@ export default class calc extends Component {
               value="PPPFunds2020No"
               checked={recievedPPPFunds2020 === 'PPPFunds2020No'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -295,6 +303,7 @@ export default class calc extends Component {
               value="BussinessFeb2020Yes"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020Yes'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -306,6 +315,7 @@ export default class calc extends Component {
               value="BussinessFeb2020No"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020No'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -324,6 +334,7 @@ export default class calc extends Component {
               value="SeasonalBusinessYes"
               checked={isSeasonalBusiness === 'SeasonalBusinessYes'}
               onChange={this.handleSeasonalBusiness}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -335,6 +346,7 @@ export default class calc extends Component {
               value="SeasonalBusinessNo"
               checked={isSeasonalBusiness === 'SeasonalBusinessNo'}
               onChange={this.handleSeasonalBusiness}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -424,6 +436,7 @@ export default class calc extends Component {
               value="BussinessFeb2020Yes"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020Yes'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -435,6 +448,7 @@ export default class calc extends Component {
               value="BussinessFeb2020No"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020No'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -479,6 +493,7 @@ export default class calc extends Component {
               value="PPPFunds2020Yes"
               checked={recievedPPPFunds2020 === 'PPPFunds2020Yes'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -490,6 +505,7 @@ export default class calc extends Component {
               value="PPPFunds2020No"
               checked={recievedPPPFunds2020 === 'PPPFunds2020No'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -508,6 +524,7 @@ export default class calc extends Component {
               value="BussinessFeb2020Yes"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020Yes'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -519,6 +536,7 @@ export default class calc extends Component {
               value="BussinessFeb2020No"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020No'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -542,6 +560,7 @@ export default class calc extends Component {
               value="Quater2019Yes"
               checked={isQuater2019 === "Quater2019Yes"}
               onChange={this.handleQuater2019}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -553,6 +572,7 @@ export default class calc extends Component {
               value="Quater2019No"
               checked={isQuater2019 === "Quater2019No"}
               onChange={this.handleQuater2019}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -591,6 +611,7 @@ export default class calc extends Component {
               value="PPPFunds2020Yes"
               checked={recievedPPPFunds2020 === 'PPPFunds2020Yes'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -602,6 +623,7 @@ export default class calc extends Component {
               value="PPPFunds2020No"
               checked={recievedPPPFunds2020 === 'PPPFunds2020No'}
               onChange={this.handleFormPPPFunds2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -620,6 +642,7 @@ export default class calc extends Component {
               value="BussinessFeb2020Yes"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020Yes'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -631,6 +654,7 @@ export default class calc extends Component {
               value="BussinessFeb2020No"
               checked={isInBussinessFeb2020 === 'BussinessFeb2020No'}
               onChange={this.handleBussinessFeb2020}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -649,6 +673,7 @@ export default class calc extends Component {
               value="Quater2019Yes"
               checked={isQuater2019 === "Quater2019Yes"}
               onChange={this.handleQuater2019}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -660,6 +685,7 @@ export default class calc extends Component {
               value="Quater2019No"
               checked={isQuater2019 === "Quater2019No"}
               onChange={this.handleQuater2019}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -678,6 +704,7 @@ export default class calc extends Component {
               value="SeasonalBusinessYes"
               checked={isSeasonalBusiness === 'SeasonalBusinessYes'}
               onChange={this.handleSeasonalBusiness}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -689,6 +716,7 @@ export default class calc extends Component {
               value="SeasonalBusinessNo"
               checked={isSeasonalBusiness === 'SeasonalBusinessNo'}
               onChange={this.handleSeasonalBusiness}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -707,6 +735,7 @@ export default class calc extends Component {
               value="FoodHospitalityIndustryYes"
               checked={isFoodHospitalityIndustry === "FoodHospitalityIndustryYes"}
               onChange={this.handleFoodHospitalityIndustry}
+              className={`${styles.radioText}`}
             />
           </Col>
           <Col>
@@ -718,6 +747,7 @@ export default class calc extends Component {
               value="FoodHospitalityIndustryNo"
               checked={isFoodHospitalityIndustry === "FoodHospitalityIndustryNo"}
               onChange={this.handleFoodHospitalityIndustry}
+              className={`${styles.radioText}`}
             />
           </Col>
         </Row>
@@ -793,7 +823,7 @@ export default class calc extends Component {
                 {loanCalcForm}
                 <Form.Group>
                   <Form.Text className={`${styles.findMore}`}>
-                      Self-employed? Find out more here.
+                    Self-employed? Find out more here.
                     <div className={`${styles.svgContainer}`} onClick={this.onShowFindMore}>
                       <svg fill="currentcolor" width="15" height="15" viewBox="0 0 24 24" className="styled-svg svg-cc42820e styled-Info Info-0 ">
                         <path d="M13,15.5 L13.5001925,15.5 C14.0523709,15.5 14.5,15.9438648 14.5,16.5 C14.5,17.0522847 14.0562834,17.5 13.5001925,17.5 L10.4998075,17.5 C9.94762906,17.5 9.5,17.0561352 9.5,16.5 C9.5,15.9477153 9.94371665,15.5 10.4998075,15.5 L11,15.5 L11,12.5 L10.5029595,12.5 C9.94904028,12.5 9.5,12.0561352 9.5,11.5 C9.5,10.9477153 9.94327742,10.5 10.5029595,10.5 L11.9970405,10.5 C12.275656,10.5 12.5252617,10.6117379 12.70615,10.7926069 C12.8881391,10.9728223 13,11.2224272 13,11.5 L13,15.5 Z M12,22 C6.4771525,22 2,17.5228475 2,12 C2,6.4771525 6.4771525,2 12,2 C17.5228475,2 22,6.4771525 22,12 C22,17.5228475 17.5228475,22 12,22 Z M12,20 C16.418278,20 20,16.418278 20,12 C20,7.581722 16.418278,4 12,4 C7.581722,4 4,7.581722 4,12 C4,16.418278 7.581722,20 12,20 Z M12,9.5 C11.1715729,9.5 10.5,8.82842712 10.5,8 C10.5,7.17157288 11.1715729,6.5 12,6.5 C12.8284271,6.5 13.5,7.17157288 13.5,8 C13.5,8.82842712 12.8284271,9.5 12,9.5 Z">
@@ -858,10 +888,10 @@ export default class calc extends Component {
                   <img 
                     src={dollarImg} 
                     alt="Illustration of a bag of money" 
-                    style={{width: 200, height: 200}}
+                    style={{width: 96, height: 96}}
                   />
                   <Form.Group>
-                    <Form.Label className={`${styles.formCalcPPPContainerLabel}`}>
+                    <Form.Label className={`${styles.formCalcPPPContainerRightLabel}`}>
                       How much you may qualify for
                     </Form.Label>
                   </Form.Group>
